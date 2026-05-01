00dafa04a96ce095bba96159d189800c2645da5d
oya <oya@peco.com>
2026-05-01 12:42:37 +0900

    結果を文字色で変更する

--- script.js
+++ script.js
@@ -10,6 +10,15 @@ const result = document.getElementById("result");
 
 button.addEventListener("click",function(){
         const randomNumber = Math.floor(Math.random() * fortunes.length);
-
+        const resultTex = fortunes[(randomNumber)]
+   
         result.textContent  = fortunes [(randomNumber)];
+
+        if(resultTex.includes("大吉")){
+           result.style.color = "red" ;    
+        } else if(resultTex.includes("中吉")){
+           result.style.color = "green" ;
+        } else {
+            result.style.color = "black";    
+        }   
 });

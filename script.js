541b3ec00e038d4199ffcfaac38661df72a2477c
oya <oya@peco.com>
2026-05-01 12:12:09 +0900

    おみくじの結果を追加

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const fortunes =[
         "大吉💖素敵な出会いがあるかも！",
         "中吉　少し勇気を出すとよいことが起きる",
         "小吉　今日はゆっくり過ごそう",
-        "凶　無理せず慎重に行動しよう"
+        "末吉　一度しっかり考えて行動するようにしよう",
+        "凶　今日はゆっくり休んで明日頑張ろう"
 ];
 const button = document.getElementById("omikuji-bt");
 const result = document.getElementById("result");
